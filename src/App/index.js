e608b2703db26e977c9348e94dd9983a75410639
romankrru <rm07ru@gmail.com>
2020-05-03 00:21:58 +0300

    Add app reducer

--- src/App/index.js
+++ src/App/index.js
@@ -1,45 +1,41 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 import { useEventListener } from "../generic/hooks";
 import Board from "./Board";
 import Header from "./Header";
 import Keyboard from "./Keyboard";
-
-const _grid = [
-	[3, 0, 6, 5, 0, 8, 4, 0, 0],
-	[5, 2, 0, 0, 0, 0, 0, 0, 0],
-	[0, 8, 7, 0, 0, 0, 0, 3, 1],
-	[0, 0, 3, 0, 1, 0, 0, 8, 0],
-	[9, 0, 0, 8, 6, 3, 0, 0, 5],
-	[0, 5, 0, 0, 9, 0, 6, 0, 0],
-	[1, 3, 0, 0, 0, 0, 2, 5, 0],
-	[0, 0, 0, 0, 0, 0, 0, 7, 4],
-	[0, 0, 5, 2, 0, 6, 3, 0, 0],
-];
-
-const EMPTY_CELL = 0;
-
-const transformGrid = (grid) => {
-	return _grid.map((row) =>
-		row.map((cell) => {
-			return {
-				value: cell === EMPTY_CELL ? null : cell,
-				changeable: cell === EMPTY_CELL,
-				candidates: [],
-			};
-		})
-	);
-};
+import reducer, { actionTypes, initialState } from "./reducer";
 
 const App = () => {
-	const [grid, setGrid] = useState(() => transformGrid(_grid));
-	const [focus, setFocus] = useState([]);
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	const setFocus = (focus) =>
+		dispatch({ type: actionTypes.SET_FOCUS, focus: focus });
+
+	const updateCell = (rowIdx, colIdx, newValue) =>
+		dispatch({
+			type: actionTypes.UPDATE_CELL,
+			rowIdx: rowIdx,
+			colIdx: colIdx,
+			data: { value: newValue },
+		});
+
+	const clearCell = (rowIdx, colIdx) =>
+		dispatch({ type: actionTypes.CLEAR_CELL, rowIdx: rowIdx, colIdx: colIdx });
+
+	const toggleCandidate = (rowIdx, colIdx, candidate) =>
+		dispatch({
+			type: actionTypes.TOGGLE_CANDIDATE,
+			rowIdx: rowIdx,
+			colIdx: colIdx,
+			candidate: candidate,
+		});
 
 	const handleKeyPress = (e) => {
-		const [focusRowIdx, focusColIdx] = focus;
+		const [focusRowIdx, focusColIdx] = state.focus;
 
 		if (["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight"].includes(e.key)) {
-			if (!focus.length) {
+			if (!state.focus.length) {
 				setFocus([0, 0]);
 				return;
 			}
@@ -66,65 +62,18 @@ const App = () => {
 			}
 		}
 
-		if (!focus.length) return;
+		if (!state.focus.length) return;
 
-		if (!grid[focusRowIdx][focusColIdx].changeable) return;
+		if (!state.grid[focusRowIdx][focusColIdx].changeable) return;
 
-		if (["1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(e.key)) {
-			// FIXME: use 'slice'
-			setGrid(
-				grid.map((row, rowIdx) => {
-					if (rowIdx !== focusRowIdx) return row;
+		if (["1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(e.key))
+			updateCell(focusRowIdx, focusColIdx, Number(e.key));
 
-					return row.map((col, colIdx) => {
-						if (colIdx !== focusColIdx) return col;
-
-						return {
-							...col,
-							value: Number(e.key),
-							candidates: [],
-						};
-					});
-				})
-			);
-		}
-
-		if (["Backspace", "Delete"].includes(e.key)) {
-			setGrid(
-				grid.map((row, rowIdx) => {
-					if (rowIdx !== focusRowIdx) return row;
-
-					return row.map((col, colIdx) => {
-						if (colIdx !== focusColIdx) return col;
-
-						return {
-							...col,
-							value: null,
-						};
-					});
-				})
-			);
-		}
+		if (["Backspace", "Delete"].includes(e.key))
+			clearCell(focusRowIdx, focusColIdx);
 	};
 
 	useEventListener("keydown", handleKeyPress);
-
-	const toggleCandidate = (rowIdx, colIdx, candidate) => {
-		return setGrid([
-			...grid.slice(0, rowIdx),
-			[
-				...grid[rowIdx].slice(0, colIdx),
-				{
-					...grid[rowIdx][colIdx],
-					candidates: grid[rowIdx][colIdx].candidates.includes(candidate)
-						? grid[rowIdx][colIdx].candidates.filter((el) => el !== candidate)
-						: grid[rowIdx][colIdx].candidates.concat(candidate),
-				},
-				...grid[rowIdx].slice(colIdx + 1),
-			],
-			...grid.slice(rowIdx + 1),
-		]);
-	};
 
 	const handleAppKeyboard = (value) => {
 		console.log(value);
@@ -135,8 +84,8 @@ const App = () => {
 			<Header />
 			<Board
 				toggleCandidate={toggleCandidate}
-				grid={grid}
-				focus={focus}
+				grid={state.grid}
+				focus={state.focus}
 				setFocus={setFocus}
 			/>
 			<Keyboard onClick={handleAppKeyboard} />
---
+++ src/App/reducer.js
@@ -0,0 +1,106 @@
+// FIXME:
+const _grid = [
+	[3, 0, 6, 5, 0, 8, 4, 0, 0],
+	[5, 2, 0, 0, 0, 0, 0, 0, 0],
+	[0, 8, 7, 0, 0, 0, 0, 3, 1],
+	[0, 0, 3, 0, 1, 0, 0, 8, 0],
+	[9, 0, 0, 8, 6, 3, 0, 0, 5],
+	[0, 5, 0, 0, 9, 0, 6, 0, 0],
+	[1, 3, 0, 0, 0, 0, 2, 5, 0],
+	[0, 0, 0, 0, 0, 0, 0, 7, 4],
+	[0, 0, 5, 2, 0, 6, 3, 0, 0],
+];
+
+const EMPTY_CELL = 0;
+
+const transformGrid = (grid) => {
+	return _grid.map((row) =>
+		row.map((cell) => {
+			return {
+				value: cell === EMPTY_CELL ? null : cell,
+				changeable: cell === EMPTY_CELL,
+				candidates: [],
+			};
+		})
+	);
+};
+
+const initialState = {
+	grid: transformGrid(_grid),
+	focus: [],
+};
+
+const actionTypes = {
+	UPDATE_CELL: "UPDATE_CELL",
+	CLEAR_CELL: "CLEAR_CELL",
+	SET_GRID: "SET_GRID",
+	TOGGLE_CANDIDATE: "TOGGLE_CANDIDATE",
+	SET_FOCUS: "SET_FOCUS",
+};
+
+const updateCell = (grid, rowIdx, colIdx, data) => {
+	return [
+		...grid.slice(0, rowIdx),
+
+		[
+			...grid[rowIdx].slice(0, colIdx),
+			{
+				...grid[rowIdx][colIdx],
+				...data,
+			},
+			...grid[rowIdx].slice(colIdx + 1),
+		],
+
+		...grid.slice(rowIdx + 1),
+	];
+};
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case actionTypes.SET_GRID:
+			return {
+				...state,
+				grid: action.grid,
+			};
+
+		case actionTypes.UPDATE_CELL:
+			return {
+				...state,
+				grid: updateCell(state.grid, action.rowIdx, action.colIdx, action.data),
+			};
+
+		case actionTypes.CLEAR_CELL:
+			return {
+				...state,
+
+				grid: updateCell(state.grid, action.rowIdx, action.colIdx, {
+					value: null,
+					candidates: [],
+				}),
+			};
+
+		case actionTypes.TOGGLE_CANDIDATE: {
+			const currentCandidates =
+				state.grid[action.rowIdx][action.colIdx].candidates;
+
+			return {
+				...state,
+
+				grid: updateCell(state.grid, action.rowIdx, action.colIdx, {
+					candidates: currentCandidates.includes(action.candidate)
+						? currentCandidates.filter((c) => c !== action.candidate)
+						: [...currentCandidates, action.candidate],
+				}),
+			};
+		}
+
+		case actionTypes.SET_FOCUS:
+			return { ...state, focus: action.focus };
+
+		default:
+			return state;
+	}
+};
+
+export { actionTypes, initialState };
+export default reducer;

3b3ce85ee0bff9bb61cb64159f5ff746f3e411ce
romankrru <rm07ru@gmail.com>
2020-05-03 13:00:35 +0300

    Remove debug

--- src/App/Board/index.js
+++ src/App/Board/index.js
@@ -6,29 +6,26 @@ import styles from "./index.module.css";
 
 const Board = (props) => {
 	const invalidCells = useMemo(() => validate(props.grid), [props.grid]);
-	console.log(invalidCells);
 
 	return (
 		<div className={styles.board}>
-			{props.grid.map((row, i) => {
-				return row.map((cell, j) => {
-					return (
-						<Cell
-							rowIdx={i}
-							colIdx={j}
-							isValid={!invalidCells.includes(`${i};${j}`)}
-							toggleCandidate={props.toggleCandidate}
-							changeable={cell.changeable}
-							isHiglighted={props.focus[0] === i || props.focus[1] === j}
-							setFocus={props.setFocus}
-							isFocus={props.focus[0] === i && props.focus[1] === j}
-							// FIXME:
-							cell={cell}
-							key={String(i) + String(j)}
-						/>
-					);
-				});
-			})}
+			{props.grid.map((row, i) =>
+				row.map((cell, j) => (
+					<Cell
+						rowIdx={i}
+						colIdx={j}
+						isValid={!invalidCells.includes(`${i};${j}`)}
+						toggleCandidate={props.toggleCandidate}
+						changeable={cell.changeable}
+						isHiglighted={props.focus[0] === i || props.focus[1] === j}
+						setFocus={props.setFocus}
+						isFocus={props.focus[0] === i && props.focus[1] === j}
+						// FIXME:
+						cell={cell}
+						key={String(i) + String(j)}
+					/>
+				))
+			)}
 		</div>
 	);
 };
